3941b0443a8bc177b76c253681bb447bd36f31f2
awalvie <agrawal.vishesh.178@gmail.com>
2023-10-28 19:11:50 +0530

    feat: add faker struct tags to contacts model

--- models/models.go
+++ models/models.go
@@ -8,13 +8,13 @@ import (
 
 type Contact struct {
 	gorm.Model
-	ID          uint `gorm:"primaryKey"`
-	FirstName   string
-	LastName    string
-	Email       string
-	Phone       string
-	Category    string
+	ID          uint   `gorm:"primaryKey" faker:"oneof: 1,2",unique`
+	FirstName   string `faker:"first_name"`
+	LastName    string `faker:"last_name"`
+	Email       string `faker:"email"`
+	Phone       string `faker:"phone_number"`
+	Category    string `faker:"oneof: A, B, C, D"`
 	LastContact time.Time
 	NextContact time.Time
-	Notes       string
+	Notes       string `faker:"paragraph"`
 }
