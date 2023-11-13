1309fb503d345b490f1c83920137b96637505307
awalvie <agrawal.vishesh.178@gmail.com>
2023-11-13 10:49:56 +0100

    refactor: send emails once a day

--- mail/mail.go
+++ mail/mail.go
@@ -6,6 +6,7 @@ import (
 	"path/filepath"
 	"strconv"
 	"text/template"
+	"time"
 
 	"github.com/awalvie/recall/config"
 	"github.com/awalvie/recall/models"
@@ -22,58 +23,60 @@ type Server struct {
 }
 
 func (s *Server) Start(a *config.App) {
-	log.Println("starting mail server")
+	for {
+		// Run once a day
+		time.Sleep(24 * time.Hour)
 
-	// Get contacts that need to be contacted today
-	contacts, err := models.GetContactsToday(a.DB)
-	if err != nil {
-		log.Println("error getting contacts to be contacted today", err)
+		log.Println("starting mail server")
+
+		// Get contacts that need to be contacted today
+		contacts, err := models.GetContactsToday(a.DB)
+		if err != nil {
+			log.Println("error getting contacts to be contacted today", err)
+		}
+
+		// Generate email from template
+		// Get the template path
+		tpath := filepath.Join(a.Config.Dirs.Templates, "*")
+
+		// Parse the teamplates
+		t := template.Must(template.ParseGlob(tpath))
+
+		// Generate struct to pass to the template
+		data := struct {
+			To       []string
+			From     string
+			Subject  string
+			Contacts []models.Contact
+		}{
+			To:       a.Config.Mail.To,
+			From:     a.Config.Mail.From,
+			Subject:  a.Config.Mail.Subject,
+			Contacts: contacts,
+		}
+
+		// Execute the template
+		var emailBody bytes.Buffer
+		err = t.ExecuteTemplate(&emailBody, "email", data)
+		if err != nil {
+			log.Println("error executing template", err)
+		}
+
+		// Remove the leading new line
+		emailBodyTrim := bytes.TrimLeft(emailBody.Bytes(), "\n")
+
+		// Server address
+		addr := s.Host + ":" + strconv.Itoa(s.Port)
+
+		// Authenticate to the mail Server
+		auth := sasl.NewPlainClient("", s.Username, s.Password)
+
+		// Send email
+		err = smtp.SendMail(addr, auth, a.Config.Mail.From, a.Config.Mail.To, bytes.NewReader(emailBodyTrim))
+		if err != nil {
+			log.Println("error sending mail", err)
+		} else {
+			log.Println("mail sent successfully")
+		}
 	}
-
-	// Generate email from template
-	// Get the template path
-	tpath := filepath.Join(a.Config.Dirs.Templates, "*")
-
-	// Parse the teamplates
-	t := template.Must(template.ParseGlob(tpath))
-
-	// Generate struct to pass to the template
-	data := struct {
-		To       []string
-		From     string
-		Subject  string
-		Contacts []models.Contact
-	}{
-		To:       a.Config.Mail.To,
-		From:     a.Config.Mail.From,
-		Subject:  a.Config.Mail.Subject,
-		Contacts: contacts,
-	}
-
-	// Execute the template
-	var emailBody bytes.Buffer
-	err = t.ExecuteTemplate(&emailBody, "email", data)
-	if err != nil {
-		log.Println("error executing template", err)
-	}
-
-	log.Println(emailBody.String())
-
-	// Remove the leading new line
-	emailBodyTrim := bytes.TrimLeft(emailBody.Bytes(), "\n")
-
-	// Server address
-	addr := s.Host + ":" + strconv.Itoa(s.Port)
-
-	// Authenticate to the mail Server
-	auth := sasl.NewPlainClient("", s.Username, s.Password)
-
-	// Send email
-	err = smtp.SendMail(addr, auth, a.Config.Mail.From, a.Config.Mail.To, bytes.NewReader(emailBodyTrim))
-	if err != nil {
-		log.Println("error sending mail", err)
-	} else {
-		log.Println("mail sent successfully")
-	}
-
 }
